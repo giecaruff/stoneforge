48e25452e041f15ddf595049b5657938b8915b0b
Rodrigo Dutra <dutrarodrigo@id.uff.br>
2021-12-06 17:52:20 -0300

    Changing the exception and message

--- stoneforge/petrophysics/shale_volume.py
+++ stoneforge/petrophysics/shale_volume.py
@@ -22,8 +22,8 @@ def gammarayindex(gr: npt.ArrayLike, grmin: float, grmax: float) -> np.ndarray:
     """
 
     if grmin == grmax:
-        msg = "The value of grmin is equal to the value of grmax. Division by zero encountered"
-        raise ValueError(msg)
+        msg = "Division by zero. The value of grmin is equal to the value of grmax."
+        raise ZeroDivisionError(msg)
 
     igr = (gr - grmin) / (grmax - grmin)
     igr = np.clip(igr, 0.0, 1.0)
