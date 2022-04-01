ce6c512da32e8b098492d391fbb52ae9318cf6ca
Yuri Alves <80852735+Yuri211200@users.noreply.github.com>
2022-04-01 17:57:32 -0300

    Update 1

--- stoneforge/tests_porosity/test_new_format.py
+++ stoneforge/tests_porosity/test_new_format.py
@@ -1,10 +1,9 @@
 # %%
 import sys
 import os
-from gevent import config
+from typing_extensions import Self
 import numpy as np
 import pytest
-from stoneforge.tests_porosity.test_improved import sorted_values
 
 if __package__:
     from ..petrophysics.porosity import porosity
@@ -33,6 +32,12 @@ class Tests:
         property_values = np.array(property_values).T
 
         return property_values,values_names
+
+    config_1 = {
+    "rhob":(0.0,1.0),
+    "rhom":(0.0,1.0),
+    "rhof":(0.0,1.0),
+    }
 
     config_2 = {
     "nphi":(0.0,1.0),
@@ -65,19 +70,43 @@ class Tests:
 
 class Density(Tests):
 
-    config_1 = {
+    def __init__ (self):
+        
+        self.config_1 = {
+        "rhob":(0.0,1.0),
+        "rhom":(0.0,1.0),
+        "rhof":(0.0,1.0),
+        }
+
+        self.density_values = self.sorted_values(self.config_1)
+
+    def values(self):
+
+        @pytest.mark.parametrize(self.density_values[1], self.density_values[0])
+        def test_density(rhob, rhom, rhof):
+            p = porosity(rhob = rhob, rhom = rhom, rhof = rhof,
+                        method = "density")
+            assert p >= 0 and p <= 1
+
+density_test = Density()
+density_test.values()
+print(density_test.density_values)
+# ---------------------------------------------------------- #
+
+""" class Neutron(Tests):
+
+    config_2 = {
     "rhob":(0.0,1.0),
     "rhom":(0.0,1.0),
     "rhof":(0.0,1.0),
     }
 
-    density_values = sorted_values(config_1)
+    neutron_values = sorted_values(config_2)
 
-    @pytest.mark.parametrize(density_values[1], density_values[0])
-    def test_density(rhob, rhom, rhof):
-        p = porosity(rhob = rhob, rhom = rhom, rhof = rhof,
-                        method = "density")
-        assert p >= 0 and p <= 1
+    @pytest.mark.parametrize(neutron_values[1], neutron_values[0])
+    def test_neutron(nphi, vsh, nphi_sh):
+        p = porosity(nphi = nphi, vsh = vsh, nphi_sh = nphi_sh,
+                        method = "neutron")
+        assert p >= 0 and p <= 1 """
 
-density_test = Density()
-density_test.sorted_values()
+# ---------------------------------------------------------- #
