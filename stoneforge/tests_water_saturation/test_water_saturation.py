e340b2633e491bcce3210e1c32c4ae8d1faae876
Yuri Alves <80852735+Yuri211200@users.noreply.github.com>
2022-04-15 17:45:28 -0300

    Definitive porosity and water saturation tests suggestion

--- stoneforge/tests_water_saturation/test_water_saturation.py
+++ stoneforge/tests_water_saturation/test_water_saturation.py
@@ -13,7 +13,8 @@ else:
 # -------------------------------------------------------------------------------------------------------------- #
 # test functions
 
-archie_values = Parameters.sorted_values(Parameters.config_water_saturarion)
+
+archie_values = Parameters.sorted_values(Parameters.config_archie)
 
 @pytest.mark.parametrize(archie_values[1], archie_values[0])
 def test_archie(rw, rt, phi, a, m, n):
@@ -23,31 +24,30 @@ def test_archie(rw, rt, phi, a, m, n):
 
 # -------------------------------------------------------------------------------------------------------------- #
 
-""" @pytest.mark.parametrize("rw, rt, phi, a, m, n", archie_values)
-def test_archie(rw, rt, phi, a, m, n):
+simandoux_values = Parameters.sorted_values(Parameters.config_simandoux)
+
+@pytest.mark.parametrize(simandoux_values[1], simandoux_values[0])
+def test_simandoux(rw, rt, phi, a, m, n, vsh, rsh):
     ws = water_saturation(rw = rw, rt = rt, phi = phi, a = a,
-                        m = m, n = n, method = "archie")
+                        m = m, n = n, vsh = vsh, rsh = rsh, method = "simandoux")
     assert ws >= 0 and ws <= 1
 
- def test_archie():
-    assert round(water_saturation(rw=0.9, rt=20, phi=0.33,
-                                a=0.62, m=2.15, n=2.0,
-                                method="archie"),2) == 0.55
+# -------------------------------------------------------------------------------------------------------------- #
 
-def test_simandoux():
-    assert round(water_saturation(rw=0.015, rt=1.0, phi=0.11,
-                                a=0.62, m=2.15, n=2.0,
-                                method="simandoux", vsh=0.33,
-                                            rsh=4.0),2) == 0.82
+indonesia_values = Parameters.sorted_values(Parameters.config_indonesia)
 
-def test_indonesia(): 
-    assert round(water_saturation(rw=17, rt=14.0, phi=0.23,
-                                a=1.00, m=1.8, n=2.0,
-                                method="indonesia", vsh=0.19,
-                                rsh=4.0),2) == 0.22
+@pytest.mark.parametrize(indonesia_values[1], indonesia_values[0])
+def test_indonesia(rw, rt, phi, a, m, n, vsh, rsh):
+    ws = water_saturation(rw = rw, rt = rt, phi = phi, a = a,
+                        m = m, n = n, vsh = vsh, rsh = rsh, method = "indonesia")
+    assert ws >= 0 and ws <= 1
 
-def test_fertl(): 
-    assert round(water_saturation(rw=0.015, rt=1.0, phi=0.11,
-                                a=0.62, m=2.15, alpha=0.30,
-                                method="fertl", vsh=0.33),2) == 0.63 """
+# -------------------------------------------------------------------------------------------------------------- #
 
+fertl_values = Parameters.sorted_values(Parameters.config_fertl)
+
+@pytest.mark.parametrize(fertl_values[1], fertl_values[0])
+def test_fertl(rw, rt, phi, a, m, alpha, vsh):
+    ws = water_saturation(rw = rw, rt = rt, phi = phi, a = a,
+                        m = m, alpha = alpha, vsh = vsh, method = "fertl")
+    assert ws >= 0 and ws <= 1
