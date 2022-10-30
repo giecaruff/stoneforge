2abf4943d0ac0f1192ff50b138da00a187c6e4a7
joaovitor2614 <jvitoralvesestrella@gmail.com>
2022-10-30 11:15:02 -0300

    Retirar formula de variação linear e mudar versão stoneforge

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name="stoneforge",
-    version="0.1.2",
+    version="0.1.3",
     author="GIECAR - UFF",
     url="https://github.com/giecaruff/stoneforge",
     description="Geophysics equations, algorithms and methods",
