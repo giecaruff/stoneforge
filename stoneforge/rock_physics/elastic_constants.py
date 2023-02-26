bd593ecfde6a176b3164b5ab5d63d918917a4b30
joaovitor2614 <jvitoralvesestrella@gmail.com>
2023-02-26 15:22:42 -0300

    Method to compute bulk modulus from rhob, vp and vs

--- stoneforge/rock_physics/elastic_constants.py
+++ stoneforge/rock_physics/elastic_constants.py
@@ -1,3 +1,35 @@
+def bulk_modulus(rhob, vp, vs):
+     """
+     Computes the bulk modulus using density, shear wave velocity and compressional wave velocity.
+
+    Parameters
+    rhob : array_like, float
+        Density data.
+
+    vp : array_like, float
+        Compressional wave velocity data.
+
+    vs : array_like, float
+        Shear wave velocity data.
+
+    Returns
+    -------
+    K : array_like, float
+        Bulk modulus data.
+    ----------
+
+    References
+    ----------
+    .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
+    Cambridge University Press.
+
+    """
+     A = vp**2 
+     B = (4/3) * (vs**2)
+     K = rhob * (A - B)
+     return K
+
+
 def poisson(method: str, **kwargs):
     """Computes the Poisson ratio using two elastic moduli.
 
