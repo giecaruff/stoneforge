397b6e75b3a7f318d7461fcfac0d7bd23b9a63aa
joaovitor2614 <jvitoralvesestrella@gmail.com>
2023-03-07 15:42:49 -0300

    1/2  Code solution to #40

--- stoneforge/rock_physics/elastic_constants.py
+++ stoneforge/rock_physics/elastic_constants.py
@@ -1,21 +1,23 @@
-def bulk_modulus(rhob, vp, vs):
+import numpy.typing as npt
+
+def bulk_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike, vs: npt.ArrayLike):
      """
      Computes the bulk modulus using density, shear wave velocity and compressional wave velocity.
 
     Parameters
     rhob : array_like, float
-        Density data.
+        Density data in g/cm³.
 
     vp : array_like, float
-        Compressional wave velocity data.
+        Compressional wave velocity data in km/s².
 
     vs : array_like, float
-        Shear wave velocity data.
+        Shear wave velocity data in km/s².
 
     Returns
     -------
     K : array_like, float
-        Bulk modulus data.
+        Bulk modulus data in Pascal unit.
     ----------
 
     References
@@ -28,6 +30,91 @@ def bulk_modulus(rhob, vp, vs):
      B = (4/3) * (vs**2)
      K = rhob * (A - B)
      return K
+
+def shear_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike):
+    """
+     Computes the shear modulus using density and s wave velocity.
+
+    Parameters
+    rhob : array_like, float
+        Density data in g/cm³.
+
+    vp : array_like, float
+        Compressional wave velocity data in km/s².
+
+    Returns
+    -------
+    U : array_like, float
+        Shear modulus data in Pascal unit.
+    ----------
+
+    References
+    ----------
+    .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
+    Cambridge University Press.
+
+    """
+    U = rhob * (vp**2)
+
+    return U
+
+def shear_wave_velocity(rhob: npt.ArrayLike, u: npt.ArrayLike):
+    """
+     Computes the shear wave velocity using density and shear modulus.
+
+    Parameters
+    rhob : array_like, float
+        Density data in g/cm³.
+
+    u: array_like, float
+        Shear modulus data in Pascal unit.
+
+    Returns
+    -------
+    VS : array_like, float
+        Shear wave velocity data km/s².
+    ----------
+
+    References
+    ----------
+    .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
+    Cambridge University Press.
+
+    """
+    VS = (rhob / u)**0.5
+
+    return VS
+
+def compressional_wave_velocity(rhob: npt.ArrayLike, u: npt.ArrayLike, k: npt.ArrayLike):
+    """
+     Computes the Compressional wave velocity using density, shear modulus  and bulk modulus.
+
+    Parameters
+    rhob : array_like, float
+        Density data in g/cm³.
+
+    u: array_like, float
+        Shear  modulus data in Pascal unit.
+
+    k: array_like, float
+        Bulk modulus data in Pascal unit.
+
+    Returns
+    -------
+    VP : array_like, float
+        Compressional wave velocity data km/s².
+    ----------
+
+    References
+    ----------
+    .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
+    Cambridge University Press.
+
+    """
+    A = k + (4/3)*u
+    VP = (A / rhob)**0.5
+
+    return VP
 
 
 def poisson(method: str, **kwargs):
