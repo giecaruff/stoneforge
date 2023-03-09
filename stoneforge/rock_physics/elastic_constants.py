1b158691cc42337dbcbf6363cefd136767a6869a
joaovitor2614 <jvitoralvesestrella@gmail.com>
2023-03-09 18:56:55 -0300

    3/5  Code solution to #40

--- stoneforge/rock_physics/elastic_constants.py
+++ stoneforge/rock_physics/elastic_constants.py
@@ -9,10 +9,10 @@ def bulk_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike, vs: npt.ArrayLike):
         Density data in g/cm³.
 
     vp : array_like, float
-        Compressional wave velocity data in km/s².
+        Compressional wave velocity data in km/s.
 
     vs : array_like, float
-        Shear wave velocity data in km/s².
+        Shear wave velocity data in km/s.
 
     Returns
     -------
@@ -40,7 +40,34 @@ def shear_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike):
         Density data in g/cm³.
 
     vp : array_like, float
-        Compressional wave velocity data in km/s².
+        Compressional wave velocity data in km/s.
+
+    Returns
+    -------
+    U : array_like, float
+        Shear modulus data in Pascal unit.
+    ----------
+
+    References
+    ----------
+    .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
+    Cambridge University Press.
+
+    """
+    U = rhob * (vp**2)
+
+    return U
+
+def shear_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike):
+    """
+     Computes the shear modulus using density and s wave velocity.
+
+    Parameters
+    rhob : array_like, float
+        Density data in g/cm³.
+
+    vp : array_like, float
+        Compressional wave velocity data in km/s.
 
     Returns
     -------
@@ -72,7 +99,7 @@ def shear_wave_velocity(rhob: npt.ArrayLike, u: npt.ArrayLike):
     Returns
     -------
     VS : array_like, float
-        Shear wave velocity data km/s².
+        Shear wave velocity data km/s.
     ----------
 
     References
@@ -85,36 +112,52 @@ def shear_wave_velocity(rhob: npt.ArrayLike, u: npt.ArrayLike):
 
     return VS
 
-def compressional_wave_velocity(rhob: npt.ArrayLike, u: npt.ArrayLike, k: npt.ArrayLike):
+
+def compressional_wave_velocity(method: str, **kwargs):
     """
-     Computes the Compressional wave velocity using density, shear modulus  and bulk modulus.
+     Computes the compressional wave velocity.
 
     Parameters
-    rhob : array_like, float
-        Density data in g/cm³.
-
-    u: array_like, float
-        Shear  modulus data in Pascal unit.
-
-    k: array_like, float
-        Bulk modulus data in Pascal unit.
-
-    Returns
-    -------
-    VP : array_like, float
-        Compressional wave velocity data in km/s².
+    Parameters
+    method : str
+        The two elastic moduli used to the estimation. Should be one of:
+            - "rhob_and_g_and_k" for density and shear moduli and bolk moduli
+            - "rhob_and_m" for density and compressional modulus
     ----------
 
     References
     ----------
     .. [1] Simm, R., & Bacon, M. (2014). Seismic Amplitude: An Interpreter's Handbook.
     Cambridge University Press.
+        if method == "k_and_g":
+        required = ["k", "g"]
+        for arg in required:
+            if arg not in kwargs:
+                msg = f"Missing required argument for method '{method}': '{arg}'"
+                raise TypeError(msg)
+        return (3*kwargs["k"] - 2*kwargs["g"]) / (6*kwargs["k"] + 2*kwargs["g"])
 
     """
-    A = k + (4/3)*u
-    VP = (A / rhob)**0.5
+    if method == "rhob_and_g_and_k":
+        required = ["rhob", "g", "k"]
+        for arg in required:
+            if arg not in kwargs:
+                msg = f"Missing required argument for method '{method}': '{arg}'"
+                raise TypeError(msg)
+        A = kwargs["k"] + (4/3)*kwargs["g"]
+        VP = (A / kwargs["rhob"])**0.5
+        return VP
+    
+    elif method == "rhob_and_m":
+        required = ["rhob", "m"]
+        for arg in required:
+            if arg not in kwargs:
+                msg = f"Missing required argument for method '{method}': '{arg}'"
+                raise TypeError(msg)
+        VP = (kwargs["m"]/kwargs["rhob"])
+        return VP
 
-    return VP
+
 
 def compressional_modulus(rhob: npt.ArrayLike, vp: npt.ArrayLike):
     """
