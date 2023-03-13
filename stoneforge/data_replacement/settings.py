63c2cdb58a8a493bbf9e07e6626565d5ac96fce7
joseaugu <91153928+joseaugu@users.noreply.github.com>
2023-03-13 17:23:05 -0300

    Update settings.py

--- stoneforge/data_replacement/settings.py
+++ stoneforge/data_replacement/settings.py
@@ -3,15 +3,46 @@ import numpy.typing as npt
 import json
 import warnings
 
+# TODO: if method is not listed do not execute and gives error to the user
+
 def saves(file, name):
     with open(name+'.json', 'w') as write_file:
         json.dump(file, write_file)
 
-def settings(method: str = "simple_linear_regression", path = ".", **kwargs):
+def settings(method: str = "linear_regression", path = ".", **kwargs):
 
-    if method == "simple_linear_regression":
-        saves(kwargs, path+"\\simple_linear_regression_settings")
+    if method == "linear_regression":
+        new_settings = {}
+        if not 'degree' in kwargs:
+            saves({'degree':1}, path+"\\polinomial_settings")
+            new_settings = kwargs
+        else:
+            saves({'degree':kwargs['degree']}, path+"\\polinomial_settings")
+            new_settings = {}
+            for k in kwargs:
+                if k != 'degree':
+                    new_settings[k] = kwargs[k]
+    
+        saves(new_settings, path+"\\linear_regression_settings")
+        
 
-    if method == "DecisionTreeClassifier":
-        saves(kwargs, path+"\\decision_tree_classifier_settings")
+    if method == "suporte_vector_regression":
+        saves(kwargs, path+"\\support_vector_settings")
+
+    if method == "decision_tree_regression":
+        saves(kwargs, path+"\\decision_tree_settings")
+
+    if method == "random_florest_regression":
+        saves(kwargs, path+"\\random_florest_settings")
+
+    if method == "xgboost_regression":
+        saves(kwargs, path+"\\xgboost_settings")
+
+    if method == "lightgbm_regression":
+        saves(kwargs, path+"\\lightgbm_florest_settings")
+
+    if method == "catboost_regression":
+        saves(kwargs, path+"\\catboost_florest_settings")
+
+
 
