9decc91251d7ddf9fe026b2ba3d32b167f21d0cd
joseaugu <joseaugustovdias@gmail.com>
2023-04-20 15:54:12 -0300

    Regression Automation

--- stoneforge/data_replacement/fit.py
+++ stoneforge/data_replacement/fit.py
@@ -5,20 +5,18 @@ import json
 import warnings
 
 
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-import lightgbm as lgb
+from sklearn.ensemble import RandomForestRegressor
+import lightgbm as LGBMRegressor
 from catboost.core import CatBoostRegressor
-
-
-
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import PolynomialFeatures
-
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBClassifier
+
 
 def saves(file, name):
     with open(name + ".pkl", "wb") as write_file:
@@ -43,9 +41,6 @@ def linear_regression_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kw
     saves(slregression, path+"\\linear_regression_fit_property")
     
 
-
-
-
 #Suporte Vector 
 def suporte_vector_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
 
@@ -62,85 +57,122 @@ def suporte_vector_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwarg
 
 
 #Decison Tree
-def decision_tree_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
+def decision_tree_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path,gs=True, **kwargs):
 
-    f = open(path + '\\decision_tree_settings.json')
+    if gs:
+        parameters = {'criterion': ['gini', 'entropy'],
+        'max_depth':[5,10,15,30,50,70,100]}
 
-    settings = json.load(f)
+        decisiontree = DecisionTreeRegressor()
 
-    decisontree = DecisionTreeClassifier(**settings)
+        bestdt = GridSearchCV(decisiontree,parameters,scoring='accuracy')
+        bestdt.fit(X,y)
+        settings = bestdt.best_params_
 
-    decisontree.fit(X, y, **kwargs)
+    if not gs:
+        f = open(path + '\\decision_tree_replacement.json')
+        settings = json.load(f)
     
-    saves(decisontree, path+"\\decision_tree_fit_property")
-
-
+    d_treer = DecisionTreeRegressor(**settings)
+    d_treer.fit(X, y, **kwargs)
+    
+    saves(d_treer, path+"\\decision_tree_fit_property")
 
 
 #Random Florest
-def random_florest_replecement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
+def random_florest_replecement(X: npt.ArrayLike, y: npt.ArrayLike, path, gs=True,**kwargs):
 
-    f = open(path + '\\random_florest_settings.json')
+    if gs:
+        parameters = {'criterion': ['gini', 'entropy'],
+        'max_depth':[5,10,15,30,50,70,100]}
 
-    settings = json.load(f)
+        randomflorest = RandomForestRegressor()
 
-    random =  RandomForestClassifier(**settings)
+        bestrf = GridSearchCV(randomflorest,parameters,scoring='accuracy')
+        bestrf.fit(X,y)
+        settings = bestrf.best_params_
 
-    random.fit(X, y, **kwargs)
+    if not gs:
+        f = open(path + '\\random_florest_settings.json')
+        settings = json.load(f)
     
-    saves(random, path+"\\random_florest_fit_property")
-
-
+    d_florestc = RandomForestRegressor(**settings)
+    d_florestc.fit(X, y, **kwargs)
+    
+    saves(d_florestc, path+"\\random_florest_fit_property")
 
 
 #XgBoost
-def xgboost_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
+def xgboost_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path,gs=False, **kwargs) -> np.ndarray:
 
-    f = open(path + '\\xgboost_settings.json')
+    if gs:
+        parameters =  {'n_estimators': [100],
+        'learning_rate': [0.5],
+        'max_depth':[5,10,15,30,50,70,100]}
 
-    settings = json.load(f)
+        xgb = XGBClassifier()
 
-    xgboost =  XGBClassifier(**settings)
+        bestxgbc = GridSearchCV(xgb,parameters,scoring='accuracy')
+        bestxgbc.fit(X,y)
+        #bestxgb = bestxgb.best_params_
+        settings = bestxgbc.best_params_
 
-    xgboost.fit(X, y, **kwargs)
+    if not gs:
+        f = open(path + '\\xgboost_settings.json')
+        settings = json.load(f)
     
-    saves(xgboost, path+"\\xgboost_fit_property") 
+    xg = XGBClassifier(**settings)
+    xg.fit(X, y, **kwargs)
 
-
-
-
+    saves(xg, path+"\\xgboost_fit_property")
 
 #LightGBM
-def lightgbm_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
+def lightgbm_replacement(X: npt.ArrayLike, y: npt.ArrayLike, path, gs=False, **kwargs):
 
-    f = open(path + '\\lightgbm_florest_settings.json')
+    if gs:
+        parameters =  {'n_estimators': [100],
+        'learning_rate': [0.5],
+        'max_depth':[5,10,15,30,50,70,100]}
 
-    settings = json.load(f)
+        lghtr = LGBMRegressor()
 
-    light =  lgb(**settings)
+        bestlight = GridSearchCV(lghtr,parameters,scoring='accuracy')
+        bestlight.fit(X,y)
+        #bestxgb = bestxgb.best_params_
+        settings = bestlight.best_params_
 
-    light.fit(X, y, **kwargs)
+    if not gs:
+        f = open(path + '\\lightgbm_florest_settings.json')
+        settings = json.load(f)
     
-    saves(light, path+"\\lightgbm_replacement_fit_property")  
+    xg = XGBClassifier(**settings)
+    xg.fit(X, y, **kwargs)
 
-
-
+    saves(xg, path+"\\ightgbm_replacement_fit_property")
 
 #CatBoost
-def catboost_replecement(X: npt.ArrayLike, y: npt.ArrayLike, path, **kwargs):
+def catboost_replecement(X: npt.ArrayLike, y: npt.ArrayLike, path,gs=False, **kwargs) -> np.ndarray:
 
-    f = open(path + '\\catboost_florest_settings.json')
+    if gs:
+        parameters =  {'n_estimators': [100,150,200],
+        'learning_rate': [0.3,0.5,0.7],
+        'max_depth':[5,10,15,30,50,70,100]}
 
-    settings = json.load(f)
+        cat = CatBoostRegressor()
 
-    cat =  CatBoostRegressor(**settings)
+        bestcatr = GridSearchCV(cat,parameters,scoring='accuracy')
+        bestcatr.fit(X,y)
+        #bestcat = bestcat.best_params_
+        settings = bestcatr.best_params_
 
-    cat.fit(X, y, **kwargs)
+    if not gs:
+        f = open(path + '\\catboost_florest_settings.json')
+        settings = json.load(f)
     
-    saves(cat, path+"\\catboost_florest_fit_property")
+    cb = CatBoostRegressor(**settings)
+    cb.fit(X, y, **kwargs)
 
-
-
+    saves(cb, path+"\\catboost_florest_fit_property")
 
 
 
@@ -175,6 +207,5 @@ def fit(X: npt.ArrayLike , y: npt.ArrayLike, method: str = "linear_regression", 
         fun = _fit_methods[method]
     if method == "cat":
         fun = _fit_methods[method]
-
-    
+        
     fun(X, y, path, **kwargs)
