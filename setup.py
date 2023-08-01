d0c3104a1bce90aa0af8a3e0b8599eaacf173151
João Pedro Reis <jp_reis@id.uff.br>
2023-08-01 16:45:55 -0300

    New changes to setup and notebook

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.8.8",
+    python_requires=">=3.9.13",
     install_requires=[
         "numpy>=1.21, <1.22.1",
         "pytest>=6.2.2, <6.2.5",
