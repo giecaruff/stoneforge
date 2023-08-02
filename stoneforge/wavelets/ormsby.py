50abcdcaaf59385b7ab04b0229bbd2733e367cf6
João Pedro Reis <jp_reis@id.uff.br>
2023-08-02 15:31:44 -0300

    Updating repository
    Deletion of files

--- stoneforge/wavelets/ormsby.py
+++ stoneforge/wavelets/ormsby.py
@@ -1,12 +1,51 @@
 #Programa que implementa computacionalmente a wavelet de Ormsby
 
-import numpy as np 
+import warnings
+from typing import Sequence, Tuple
+
+import numpy as np
+import numpy.typing as npt
+
 import matplotlib.pyplot as plt
 
-def getOrmsby(f,Duration, Sampling):
+
+def _tcrop(t: npt.ArrayLike) -> npt.ArrayLike:
+    """Crop time axis with even number of samples"""
+    if len(t) % 2 == 0:
+        t = t[:-1]
+        warnings.warn("one sample removed from time axis...")
+    return t
+
+def getOrmsby(f: Sequence[float] = (5,10,40,50),
+              Samples: float = 71, 
+              Dt: float = 4) -> Tuple[npt.ArrayLike, npt.ArrayLike]:
+    r"""Ormsby wavelet
+
+    Create a Ormsby wavelet given time axis ``t`` and frequency range
+    defined by four frequencies which parametrize a trapezoidal shape in
+    the frequency spectrum.
+
+    Parameters
+    ----------
+    f : :obj:`tuple`, optional
+        Frequency range
+    Samples : :obj:`float`, optional
+        Number of samples       
+    Dt : obj:`float`, optional
+        Sampling in milisseconds
+
+    Returns
+    -------
+    wav : :obj:`numpy.ndarray`
+        Wavelet
+    t : :obj:`numpy.ndarray`
+        Symmetric time axis
+    """
     assert len(f) == 4, 'Ormsby wavelet needs 4 frequencies as input'
     f = np.sort(f) #Ormsby wavelet frequencies must be in increasing order
-    t = np.linspace(-Duration/2,Duration/2,int(Duration/Sampling))
+    t = np.arange(Samples)*(Dt/1000)
+    t = _tcrop(t)
+    t = np.concatenate((np.flipud(-t[1:]), t), axis=0)
     pif   = np.pi*f
     den1  = pif[3] - pif[2]
     den2  = pif[1] - pif[0]
@@ -16,7 +55,8 @@ def getOrmsby(f,Duration, Sampling):
     wav   = term1/den1 - term2/den2;
     wav /= np.amax(wav)
     return t, wav
-time, wvlt = getOrmsby((5,10,40,50),0.5,0.004)
+
+time, wvlt = getOrmsby()
 plt.plot(time,wvlt)
 plt.xlabel('Tempo (s)')
 plt.ylabel('Ondaleta (w)')
