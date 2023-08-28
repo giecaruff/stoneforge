04d370f609a938a551c48f602998cae232fde04b
joseaugu <91153928+joseaugu@users.noreply.github.com>
2023-08-28 12:09:52 -0300

    att

--- stoneforge/tests_ml/all_methods_test_replacement.py
+++ stoneforge/tests_ml/all_methods_test_replacement.py
@@ -64,14 +64,14 @@ print(np.shape(mega_data))
 
 # %%
 
-data_replacement.settings(method = "linear_regression_simple", path='_ml_project') # for the case there is polynomial 1D
+#data_replacement.settings(method = "linear_regression_simple", path='_ml_project') # for the case there is polynomial 1D
 #data_replacement.settings(method = "linear_regression", path='_ml_project', degree = 2)
 #data_replacement.settings(method = "DecisionTreeClassifier", path='_ml_project')
 #data_replacement.settings(method = "SVM", path='_ml_project')
 #data_replacement.settings(method = "LogisticRegression", path='_ml_project')
 #data_replacement.settings(method = "KNeighborsClassifier", path='_ml_project')
 #data_replacement.settings(method = "RandomForestClassifier", path='_ml_project')
-#data_replacement.settings(method = "xgboost_regression", path='_ml_project')
+data_replacement.settings(method = "xgboost_regression", path='_ml_project')
 
 
 # %%
@@ -87,13 +87,13 @@ print(y)
 
 # %%
 
-data_replacement.fit(X,y,method = "linear_regression_simple", path = "_ml_project")
+#data_replacement.fit(X,y,method = "linear_regression_simple", path = "_ml_project")
 #data_replacement.fit(X,y,method = "decision_tree", gs = True, path = "_ml_project")
 #data_replacement.fit(X,y,method = "SVM", path = "_ml_project")
 #data_replacement.fit(X,y,method = "LogisticRegression", path = "_ml_project")
 #data_replacement.fit(X,y,method = "KNeighborsClassifier", path = "_ml_project")
 #data_replacement.fit(X,y,method = "RandomForestClassifier", path = "_ml_project")
-#data_replacement.fit(X,y,method = "xgboost_regression", path = '_ml_project')
+data_replacement.fit(X,y,method = "xgboost_regression", path = '_ml_project')
 
 # %%
 import matplotlib.pyplot as plt
@@ -108,7 +108,7 @@ for w in xy_raw:
     x_db[w] = np.delete(x_db[w],(0), axis=1)
     print(x_db)
     print(y_db)
-    ym_db = data_replacement.predict(x_db[w], method = "linear_regression_simple", path = "_ml_project")
+    ym_db = data_replacement.predict(x_db[w], method = "xgboost_regression", path = "_ml_project")
     plt.plot(ym_db, y_db[w],'.')
     plt.grid()
     plt.show()
@@ -128,83 +128,8 @@ for well in x_db:
     #class_db[well] = machine_learning.predict(x_db[well], method = "XGBClassifier", path = "_ml_project")
     #class_db[well] = machine_learning.predict(x_db[well], method = "CatBoostClassifier", path = "_ml_project")
 
-pre_pros.return_curve(class_db)
+class_db_full = pre_pros.return_curve(class_db)
+print(class_db_full)
 # %%
 
-#a = preprocessing.predict_processing(vw_data,'data')
 
-for w in vw_data:
-    x = np.delete(vw_data[w]['data'].T,(1), axis=1)
-    y = vw_data[w]['data'].T[:,1]
-    print(len(y))
-    is_nan_y = []
-    is_not_nan_x = []
-    for i in range(len(y)):
-        if np.isnan(y[i]):
-            is_nan_y.append(i)
-            #print(i)
-        if not np.isnan(x[i]).any():
-            is_not_nan_x.append(i)
-    is_nan_y = set(is_nan_y)
-    is_not_nan_x = set(is_not_nan_x)
-
-    l_data = vw_data[w]['data']
-    c_data = a._remove_dummies(l_data)
-
-    intersection = is_not_nan_x.intersection(is_nan_y)
-    print(list(intersection))
-
-    n_x_data = []
-    y_original = []
-    for i in intersection:
-        y_original.append(y[i])
-        n_x_data.append(x[i])
-    predictment = data_replacement.predict(n_x_data, method = "xgboost_regression", path = "_ml_project")
-    print("RHOB",len(predictment))
-    print(len(c_data[:,1]))
-    break
-
-# %%
-
-print(predictment)
-
-# %%
-x_r = a.matrix_values()
-
-y_vdb = {}
-x_db = {}
-for well in x_r:
-
-    print(np.shape(x_r[well]))
-    y_vdb[well] = x_r[well][:,0]
-    x_db[well] = np.delete(x_r[well],(0), axis=1)
-    print(x_db)
-    break
-
-
-# %%
-
-class_db = {}
-
-for x in x_db:
-    class_db[x] = data_replacement.predict(x_db[x], method = "GaussianNB", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "DecisionTreeClassifier", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "SVM", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "LogisticRegression", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "KNeighborsClassifier", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "RandomForestClassifier", path = "_ml_project")
-    class_db[x] = data_replacement.predict(x_db[x], method = "XGBClassifier", path = "_ml_project")
-
-a.return_curve(class_db)
-
-
-# %%
-
-data_replacement.settings(method = "GaussianNB")
-data_replacement.fit(X,y,method = "GaussianNB")
-
-for x in x_db:
-    class_db[x] = data_replacement.predict(x_db[x], method = "GaussianNB")
-    
-
-# %%
