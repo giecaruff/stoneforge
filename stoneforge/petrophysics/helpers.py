7a8d2b200ada91a1ecf2642825e6ed75ce9bee2f
joaovitor2614 <jvitoralvesestrella@gmail.com>
2023-10-15 11:25:35 -0300

    Code solution to  #67

--- stoneforge/petrophysics/helpers.py
+++ stoneforge/petrophysics/helpers.py
@@ -2,6 +2,6 @@
 # Ensure petrophysic output data (water saturation, shale volue and porosity) be at range [0, 1]
 def correct_petrophysic_estimation_range(petrophysics_data):
     petrophysics_data[petrophysics_data > 1] = 1
-    petrophysics_data[petrophysics_data < 0] = 1
+    petrophysics_data[petrophysics_data < 0] = 0
 
     return petrophysics_data
