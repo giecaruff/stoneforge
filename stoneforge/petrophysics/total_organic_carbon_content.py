7eeea65bb2018e221f592507815e7fcb58e6986f
joaovitor2614 <jvitoralvesestrella@gmail.com>
2023-12-18 10:54:18 -0300

    Code solution to  #72

---
+++ stoneforge/petrophysics/total_organic_carbon_content.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+
+def sonic_porosity(dt, logrt, dtbaseline, logrtbaseline, lom=10.6):
+
+    """Estimate the Total Organic Carbon Content by Passey method using Sonic log and Resistivy log _.
+
+    Parameters
+    ----------
+    dt : array_like
+        Sonic log reading (acoustic transit time (μsec/ft))
+    logrt : array_like
+        Resistivity log reading (formation resistivity (ohm/m))
+    dtbaseline : int, float
+        Sonic log base line (μsec/ft)
+    logrtbaseline : int, float
+        Resistivity log base line (ohm/m)
+    lom : int, float
+        Level of maturity
+              
+    Returns
+    -------
+    TOC : array_like
+        Total organic carbon content calculated from passey method.
+
+    References
+    ----------
+    Passey, O.R., F.U. Moretti, and J.D. Stroud, 1990, A practical modal for organic richness from porosity and resistivity logs: AAPG Bulletin, v. 
+    74, p. 1777–1794.
+
+
+    """
+    dlogrt = (logrt - logrtbaseline) + 0.02*(dt - dtbaseline)
+    toc = dlogrt*10**(2.297 - 0.1688*lom)
+    return np.clip(toc, 0.0, 100.0)
