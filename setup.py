e96309bb3f2f98036a72ce8403df1def7fbedf5d
mario_martins <mmramos86@gmail.com>
2025-01-15 15:33:02 -0300

    updates for  the stoneforge 0.1.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name="stoneforge",
-    version="0.1.6",
+    version="0.1.7.dev1",
     author="GIECAR - UFF",
     url="https://github.com/giecaruff/stoneforge",
     description="Geophysics equations, algorithms and methods",
@@ -19,18 +19,18 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    python_requires="==3.12.6",
+    python_requires=">=3.8.8",
     install_requires=[
-        "numpy==1.26.4", # 05/02/2024
-        "pytest==8.1.1", # 09/03/2024
-        "scipy==1.13.0", # 02/04/2024
-        "scikit-learn==1.4.2", # 09/04/2024
-        #"notebook==7.1.3", # 18/04/2024
-        "xgboost==2.0.3", # 19/12/2023
-        "matplotlib==3.8.4", # 09/04/2024
-        "pandas==2.2.2", # 10/04/2024
-        "catboost==1.2.5", # 18/04/2024
-        "lightgbm==4.3.0", # 26/01/2024
+        "numpy>=1.26.4", # 05/02/2024
+        "pytest==8.3.4", # 01/12/2024
+        "scipy>=1.15.0", # 03/01/2025
+        "scikit-learn>=1.6.0", # 09/12/2024
+        "notebook==7.3.2", # 21/12/2024
+        "xgboost>=2.1.3", # 26/11/2024
+        "matplotlib>=3.10.0", # 14/12/2024
+        "pandas>=2.2.3", # 20/09/2024
+        "catboost>=1.2.7", # 07/09/2024
+        "lightgbm==4.5.0", # 26/07/2024
         #"auto-sklearn==0.12.5"
     ],
     long_description_content_type='text/markdown',
---
+++ stoneforge/data_management/las_import.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+def import_folder(self,ext = '.las'):
+
+    # ------------------------------------ #
+    # all paths 
+    files = []
+    # r=root, d=directories, f = files
+    for r, _, f in os.walk(self.data_path):
+        for file in f:
+            if ext in file:
+                files.append(os.path.join(r, file))
+
+    c_resumo = self.data_path+'\\'
+
+
+    for i in files:
+        n1 = i.replace(c_resumo, '')
+        self.well_names_paths[n1.replace(ext,'')] = i
