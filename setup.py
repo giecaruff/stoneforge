e7d85da35bcfd82f7c1d8b8eef7ab97b55234a5f
mario_martins <mmramos86@gmail.com>
2025-03-19 14:32:54 -0300

    begining the update of datasets

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name="stoneforge",
-    version="0.1.6-beta.1",
+    version="0.1.6-beta.2",
     author="GIECAR - UFF",
     url="https://github.com/giecaruff/stoneforge",
     description="Geophysics equations, algorithms and methods",
@@ -19,20 +19,17 @@ setup(
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.10",
+    python_requires=">=3.10", # 01/03/2021
     install_requires=[
-        "dlisio>=1.0.3", # 21/01/2025
-        "numpy>=1.26.4", # 05/02/2024
-        "pytest==8.3.4", # 01/12/2024
-        "scipy>=1.15.0", # 03/01/2025
-        "scikit-learn>=1.6.0", # 09/12/2024
-        "notebook==7.3.2", # 21/12/2024
-        "xgboost>=2.1.3", # 26/11/2024
-        "matplotlib>=3.10.0", # 14/12/2024
-        "pandas>=2.2.3", # 20/09/2024
-        "catboost>=1.2.7", # 07/09/2024
-        "lightgbm==4.5.0", # 26/07/2024
-        #"auto-sklearn==0.12.5"
+        "dlisio>=1.0.3",
+        "numpy>=2.2.0",
+        "pytest>=8.3.4",
+        "scipy>=1.14.1",
+        "scikit-learn>=1.6.1",
+        "xgboost>=2.1.4",
+        "matplotlib>=3.10.0",
+        "pandas>=2.2.2",
+        "lightgbm>=4.5.0"
     ],
     long_description_content_type='text/markdown',
 )
